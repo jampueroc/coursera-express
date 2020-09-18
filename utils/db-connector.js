6019ce6eaa03d3186a8cf02a6661d81af1a0bedc
Jorge Ampuero <jorge.ampuero.1a@gmail.com>
2020-09-18 00:47:41 -0300

    updating name of db

--- utils/db-connector.js
+++ utils/db-connector.js
@@ -4,7 +4,7 @@ class DbConnector {
     static default =new DbConnector();
     db;
     connect(){
-        mongoose.connect('mongodb://localhost/db');
+        mongoose.connect('mongodb://localhost/red_bicicletas');
         this.db = mongoose.connection;
         return new Promise(((resolve, reject) => {
             this.db.on('error', (err)=> {
